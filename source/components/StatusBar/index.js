cd29732bfbe5033f143bda7dc6397c8fcf518cc1
just-a-man <avsmirn@gmail.com>
2019-08-19 22:15:49 +0300

    hw lesson4 completed

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -1,24 +1,25 @@
 //Core
 import React, {Component} from 'react';
 
+//Components
+import { Consumer } from 'components/HOC/withProfile';
+
 //Instruments
 import Styles from './styles.m.css';
 
 export default class StatusBar extends Component {
     render () {
-        const {
-            avatar,
-            currentUserFirstName,
-            currentUserLastName,
-        } = this.props;
-
         return (
-            <section className = { Styles.statusBar }>
-                <button>
-                    <img src = { avatar } />
-                    <span>{ `${currentUserFirstName} ${currentUserLastName}` }</span>
-                </button>
-            </section>
+            <Consumer>
+                {(context) => (
+                    <section className = { Styles.statusBar }>
+                        <button>
+                            <img src = { context.avatar } />
+                            <span>{ `${context.currentUserFirstName} ${context.currentUserLastName}` }</span>
+                        </button>
+                    </section>
+                )}
+            </Consumer>
         );
     }
 }
