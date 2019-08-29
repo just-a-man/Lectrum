7baa3bffbf14c8605adf53aace8f4af1538e0e65
just-a-man <avsmirn@gmail.com>
2019-08-30 01:06:57 +0300

    video 2 completed: added like

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,5 +1,6 @@
 //Core
 import React, {Component} from 'react';
+import moment from 'moment';
 
 //Components
 import StatusBar from 'components/StatusBar';
@@ -9,16 +10,77 @@ import Spinner from 'components/Spinner';
 
 //Instruments
 import Styles from './styles.m.css';
+import { getUniqueID, delay } from 'instruments';
 
 
 export default class Feed extends Component {
+    constructor () {
+        super();
+
+        this._createPost = this._createPost.bind(this);
+        this._setPostFetchingState = this._setPostFetchingState.bind(this);
+        this._likePost = this._likePost.bind(this);
+    }
+
     state = {
         posts: [
-            {id: '1', comment: 'The first comment', created: 1526825076849},
-            {id: '2', comment: 'The second comment', created: 1526825098999},
+            {id: '1', comment: 'The first comment', created: 1526825076849, likes: []},
+            {id: '2', comment: 'The second comment', created: 1526825098999, likes: []},
         ],
-        isSpinning: true,
+        isSpinning: false,
     };
+
+    _setPostFetchingState (state) {
+        this.setState({
+            isSpinning: state,
+        });
+    }
+
+    async _createPost (comment) {
+        this._setPostFetchingState(true);
+
+        const post = {
+            id:      getUniqueID(),
+            created: moment().utc(),
+            comment,
+        };
+
+        await delay(1200);
+        this.setState(({ posts }) => ({
+            posts:      [ post, ...posts ],
+            isSpinning: false,
+        }));
+    }
+
+    async _likePost (id) {
+        const { currentUserFirstName, currentUserLastName} = this.props;
+        this._setPostFetchingState(true);
+
+        await delay(1200);
+
+        // eslint-disable-next-line react/no-access-state-in-setstate
+        const newPosts = this.state.posts.map((post) => {
+            if (post.id === id) {
+                return {
+                    ...post,
+                    likes: [
+                        {
+                            id:        getUniqueID(),
+                            firstName: currentUserFirstName,
+                            lastName:  currentUserLastName,
+                        },
+                    ],
+                };
+            }
+
+            return post;
+        });
+
+        this.setState({
+            posts:      newPosts,
+            isSpinning: false,
+        });
+    }
 
     render () {
         const { posts, isSpinning } = this.state;
@@ -28,6 +90,7 @@ export default class Feed extends Component {
                 <Post
                     key = { post.id }
                     { ...post }
+                    _likePost = { this._likePost }
                 />
             );
         });
@@ -36,7 +99,7 @@ export default class Feed extends Component {
             <section className = { Styles.feed }>
                 <Spinner isSpinning = { isSpinning } />
                 <StatusBar />
-                <Composer />
+                <Composer _createPost = { this._createPost } />
                 {postsJSX}
             </section>
         );
