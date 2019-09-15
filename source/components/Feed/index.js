850f3e3f31ff075c88deacc49f88056f13812652
just-a-man <avsmirn@gmail.com>
2019-09-15 17:30:01 +0300

    hw-lesson9 is done

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,5 +1,7 @@
 //Core
 import React, {Component} from 'react';
+import { Transition } from 'react-transition-group';
+import { fromTo } from 'gsap';
 
 //Components
 import { withProfile } from 'components/HOC/withProfile';
@@ -8,6 +10,7 @@ import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
 import Spinner from 'components/Spinner';
+import Postman from 'components/Postman';
 
 //Instruments
 import Styles from './styles.m.css';
@@ -101,61 +104,88 @@ export default class Feed extends Component {
         });
     }
 
-     _createPost = async (comment) => {
-         this._setPostFetchingState(true);
+    _createPost = async (comment) => {
+        this._setPostFetchingState(true);
 
-         const response = await fetch(api, {
-             method:  'POST',
-             headers: {
-                 'Content-Type': 'application/json',
-                 Authorization:  TOKEN,
-             },
-             body: JSON.stringify({comment}),
-         });
+        const response = await fetch(api, {
+            method:  'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization:  TOKEN,
+            },
+            body: JSON.stringify({comment}),
+        });
 
-         const { data: post } = await response.json();
-         this.setState(({ posts }) => {
-             return {
-                 posts:      [ post, ...posts ],
-                 isSpinning: false,
-             };
-         });
-     }
+        const { data: post } = await response.json();
+        this.setState(({ posts }) => {
+            return {
+                posts:      [ post, ...posts ],
+                isSpinning: false,
+            };
+        });
+    }
 
-      _deletePost = async (id) => {
-          this._setPostFetchingState(true);
+    _deletePost = async (id) => {
+        this._setPostFetchingState(true);
 
-          await fetch(`${api}/${id}`, {
-              method:  'DELETE',
-              headers: {
-                  Authorization: TOKEN,
-              },
-          });
+        await fetch(`${api}/${id}`, {
+            method:  'DELETE',
+            headers: {
+                Authorization: TOKEN,
+            },
+        });
 
-          this.setState(({ posts }) => ({
-              posts:      posts.filter((post) => post.id !== id),
-              isSpinning: false,
-          }));
+        this.setState(({ posts }) => ({
+            posts:      posts.filter((post) => post.id !== id),
+            isSpinning: false,
+        }));
+    };
+
+    _likePost = async (id) => {
+        this._setPostFetchingState(true);
+
+        const response = await fetch(`${api}/${id}`, {
+            method:  'PUT',
+            headers: {
+                Authorization: TOKEN,
+            },
+        });
+
+        const { data: likedPost} = await response.json();
+
+        this.setState(({ posts }) => ({
+            posts: posts.map(
+                (post) => post.id === likedPost.id ? likedPost : post,
+            ),
+            isSpinning: false,
+        }));
+    };
+
+      _animateComposerEnter = (composer) => {
+          fromTo(
+              composer,
+              1,
+              { opacity: 0, rotationX: 50 },
+              { opacity: 1, rotationX: 0 },
+          );
       };
 
-      _likePost = async (id) => {
-          this._setPostFetchingState(true);
+      _animatePostmanEnter = (postman) => {
+          fromTo(
+              postman,
+              2,
+              { opacity: 0 },
+              { opacity: 1 },
+          );
+      };
 
-          const response = await fetch(`${api}/${id}`, {
-              method:  'PUT',
-              headers: {
-                  Authorization: TOKEN,
-              },
-          });
-
-          const { data: likedPost} = await response.json();
-
-          this.setState(({ posts }) => ({
-              posts: posts.map(
-                  (post) => post.id === likedPost.id ? likedPost : post,
-              ),
-              isSpinning: false,
-          }));
+      _animatePostmanEntered = (postman) => {
+          fromTo(
+              postman,
+              1,
+              { opacity: 1 },
+              { opacity: 0 },
+          );
       };
 
       render () {
@@ -177,7 +207,22 @@ export default class Feed extends Component {
               <section className = { Styles.feed }>
                   <Spinner isSpinning = { isSpinning } />
                   <StatusBar />
-                  <Composer _createPost = { this._createPost } />
+                  <Transition
+                      appear
+                      in
+                      timeout = { 1000 }
+                      onEnter = { this._animateComposerEnter }>
+                      <Composer _createPost = { this._createPost } />
+                  </Transition>
+                  <Transition
+                      appear
+                      in
+                      timeout = { 4000 }
+                      onEnter = { this._animatePostmanEnter }
+                      onEntered = { this._animatePostmanEntered }>
+                      <Postman />
+                  </Transition>
+
                   {postsJSX}
               </section>
           );
